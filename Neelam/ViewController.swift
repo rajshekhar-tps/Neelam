7e63e83f9351133a03f84bd1d68b915d9364f04a
Raj <rajshekharsahu@gmail.com>
2020-06-12 12:37:28 +0530

    Hex String Conversion

--- Neelam/ViewController.swift
+++ Neelam/ViewController.swift
@@ -15,9 +15,67 @@ class ViewController: UIViewController {
         print("The color is now blue")
         
         print(reverse(text: "stressed"))
+        
+        let str = "Hello"
+        let data = Data(str.utf8)
+        let hexString = data.map{ String(format:"%02x", $0) }.joined()
+        
+        print("hexString >> \(hexString)")
+        
+        
+       let d = Data(fromHexEncodedString: hexString)!
+       let s = String(data: d, encoding: .isoLatin1)
+        
+        print("String from Hex >> \(s)")
     }
 
 
+}
+
+extension Data {
+
+// From http://stackoverflow.com/a/40278391:
+init?(fromHexEncodedString string: String) {
+
+    // Convert 0 ... 9, a ... f, A ...F to their decimal value,
+    // return nil for all other input characters
+    func decodeNibble(u: UInt16) -> UInt8? {
+        switch(u) {
+        case 0x30 ... 0x39:
+            return UInt8(u - 0x30)
+        case 0x41 ... 0x46:
+            return UInt8(u - 0x41 + 10)
+        case 0x61 ... 0x66:
+            return UInt8(u - 0x61 + 10)
+        default:
+            return nil
+        }
+    }
+
+    self.init(capacity: string.utf16.count/2)
+    var even = true
+    var byte: UInt8 = 0
+    for c in string.utf16 {
+        guard let val = decodeNibble(u: c) else { return nil }
+        if even {
+            byte = val << 4
+        } else {
+            byte += val
+            self.append(byte)
+            
+            
+            }
+                        even = !even
+                    }
+                    guard even else { return nil }
+                }
+            }
+            
+
+func convertHexStringToNormalString(hexString:String)->String!{
+  if let data = hexString.data(using: .utf8){
+      return String.init(data:data, encoding: .utf8)
+  }else{ return nil}
 }
 
 func reverse(text: String) -> String {
